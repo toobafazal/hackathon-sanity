7bf71f7d11d1875e1a711361fa5e64d045d8d62e
toobafazal <toobafazal75@gmail.com>
2023-08-23 23:57:44 +0500

    Product API

--- src/app/allProducts/page.tsx
+++ src/app/allProducts/page.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import FetchData from '../../../sanity/FetchData'
+import { urlForImage } from '../../../sanity/lib/image'
 
 const Product = async () => {
   const data = await FetchData()
-  console.log(data,"dataaa")
+  console.log(data[0].image, "dataaa")
   return (
-    <div>index</div>
+    <>
+        <div>All Products</div>
+      <div className='flex gap-5'>
+        {data.map((product: any) =>
+          <>
+          <div>
+            <img src={urlForImage(product.image).url()} alt={product.title} width={400} height={400} />
+            <h1>{product.title}</h1>
+            <h1>{product.description}</h1>
+            <h1>{product.category}</h1>
+            <h1>{product.price}</h1>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
